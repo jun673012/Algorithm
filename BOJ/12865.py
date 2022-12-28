2f9e3d9b7ae5ceb3ed7158effb0155590ccc056b
jun673012 <jun673012@gmail.com>
2022-12-28 17:00:21 +0900

    BOJ 12865

---
+++ BOJ/12865.py
@@ -0,0 +1,9 @@
+N, K = map(int, input().split())
+dp = [0 for _ in range(K+1)]
+
+for _ in range(N):
+  W, V = map(int, input().split())
+  for i in range(K, W-1, -1):
+    dp[i] = max(dp[i], dp[i-W] + V)
+
+print(dp[-1])
