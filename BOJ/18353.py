df44c71662093c062b10c9a5fc8a0d9558691da5
Junhyeok Seo <jun673012@gmail.com>
2023-01-23 16:09:08 +0900

    .

---
+++ BOJ/18353.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+array = list(map(int, input().split()))
+
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if array[i] < array[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(len(array) - max(dp))
