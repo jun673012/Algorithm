8204db70c1a04d37c1ff1ff1212f64301fc02680
Junhyeok Seo <jun673012@gmail.com>
2023-01-25 14:38:00 +0900

    .

---
+++ 2501.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+res = []
+
+for i in range(1, n + 1):
+  if n % i == 0:
+    res.append(i)
+    
+if len(res) < k:
+  print(0)
+else:
+  print(res[k -1 ])
