7e192ce284f99a4e546c0a6e1b3108d17c34cce9
Junhyeok Seo <jun673012@gmail.com>
2023-01-31 16:02:49 +0900

    .

---
+++ BOJ/11286.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+heap = []
+
+n = int(input())
+
+for i in range(n):
+  x = int(input())
+  if x != 0:
+    heapq.heappush(heap, (abs(x), x))
+  else:
+    if not heap:
+      print(0)
+    else:
+      print(heapq.heappop(heap)[1])
