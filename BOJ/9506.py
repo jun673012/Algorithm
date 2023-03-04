35559f6f28c58e34d2b05c50a09f6f32af7f3750
Junhyeok Seo <jun673012@gmail.com>
2023-03-04 21:19:45 +0900

    .

---
+++ BOJ/9506.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+  n = int(input())
+  a = []
+  if n == -1:
+    break
+  for i in range(1, n):
+    if n%i == 0:
+      a.append(i)
+  if sum(a) == n:
+    print(n, ' = ', ' + '.join(str(i) for i in a), sep='')
+  else:
+    print(n, 'is NOT perfect.')
