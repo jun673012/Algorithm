dcca6d0dd7e0c647a00dd44febc44170c486a562
Junhyeok Seo <jun673012@gmail.com>
2023-06-01 23:31:23 +0900

    .

---
+++ BOJ/9237.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+t = list(map(int, input().split()))
+t.sort(reverse=True)
+
+for i in range(len(t)):
+  t[i] = t[i]+i+1
+  
+print(max(t)+1)
